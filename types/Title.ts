bc021b61f2c589db5e22f0eb3225a68ed3f74036
LazySelkie <54956262+LazySelkie@users.noreply.github.com>
2025-04-11 22:53:24 +0300

    erd stuff

--- types/Title.ts
+++ types/Title.ts
@@ -1,20 +1,21 @@
 export interface Title {
-    name: string,
-    engName: string,
-    src: string,
-    information: {
-      type: string,
-      volume: number, // Число томов
-      chapters: number, // число глав
-      status: string,
-      genres: string,
-      theme: string,
-      license?: string,
-      altName?: string,
-    },
-    description?: string,
-    publisher?: string,
-    review: number, // Рейтинг тайтла
-    related?: string, // Подумать как сделать
-    authors?: string,
+  id: number,
+  name: string,
+  engName: string,
+  image: string, // TODO: ?понять как картинки хранить и отдавать
+  information: {
+    type: string,
+    volume: number, // Число томов
+    chapters: number, // число глав
+    status: string,
+    genres: string,
+    theme: string,
+    license?: string,
+    altName?: string,
+  },
+  description?: string,
+  publisher?: string,
+  review: number, // Рейтинг тайтла
+  related?: string, // Подумать как сделать
+  authors?: string,
 };
